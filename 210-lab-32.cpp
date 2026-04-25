6ebaac9b1169e7f77706312ec928b25fdc6709fc
dwoo27 <dwooed27@gmail.com>
2026-04-24 21:17:59 -0700

    final push

--- 210-lab-32.cpp
+++ 210-lab-32.cpp
@@ -20,26 +20,31 @@ int main()
         line.push_back(nextCar);
     }
 
-    int period = 1;
+    cout << "Intial" << endl;
+    displayQueue(line);
+    cout << endl;
 
-    while (!line.empty()) {
-        int prob = rand() % 100 + 1;
+    int period = 1; //counter for periods
+
+    while (!line.empty()) { //run while line is not empty
+        int prob = rand() % 100 + 1; //produces rand prob
 
         cout << "Time: " << period << " Operation: ";
 
-        if (prob <= 55) {
+        if (prob <= 55) { //55% prob car in front pays
             cout << "Car paid: ";
-            line.front().print();
-            line.pop_front();
+            line.front().print(); //peek front and print
+            line.pop_front(); //remove from front
             cout << endl;
         }
-        else{
+        else{ //remaining 45% prob car joins
             cout << "Joined lane: ";
-            Car nextCar;
-            line.push_back(nextCar);
+            Car nextCar; //create new car
+            line.push_back(nextCar); //put in back of deque
             nextCar.print();
             cout << endl;
         }
+
         displayQueue(line);
         cout << endl;
         period++;
