83430c7ebaf55c3bd851d8e23026ba2cbb793a87
dwoo27 <dwooed27@gmail.com>
2026-04-24 20:45:58 -0700

    10 min commit

--- 210-lab-32.cpp
+++ 210-lab-32.cpp
@@ -1,9 +1,30 @@
-
-
 #include <iostream>
+#include <deque>
+#include <cstdlib>
+#include <ctime>
+#include "Car.h"
+using namespace std;
+
+const int INTIAL_SIZE = 2;
+
+void displayQueue(deque<Car>&);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
+void displayQueue(deque<Car>& line) {
+    cout << "Queue:" << endl;
+
+    if (line.empty()) {
+        cout << "\tEmpty" << endl;
+    }
+    else {
+        for (Car& car : line) {
+            cout << "\t";
+            car.print();
+            cout << endl;
+        }
+    }
+}
