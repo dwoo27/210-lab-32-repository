0c0bb24a32c16ca782e1b41b8b23e1b263bba6b1
dwoo27 <dwooed27@gmail.com>
2026-04-25 00:50:43 -0700

    Milestone 5 complete final push

--- 210-lab-32.cpp
+++ 210-lab-32.cpp
@@ -50,6 +50,9 @@ int main()
                     cout << endl;
 
                 }
+                else {
+                    cout << "Empty" << endl << endl;
+                }
             }
             else {
                 if (prob <= PAID) { //prob paid 46%, prob is 1-46
@@ -80,44 +83,18 @@ int main()
                     switching.print();
                     cout << endl;
                 }
-            }   
-
-            displayPlaza(lanes);
-            cout << endl;
-
-        }
-    }
-    /* int period = 1; //counter for periods
-
-    while (!line.empty()) { //run while line is not empty
-        int prob = rand() % 100 + 1; //produces rand prob
-
-        cout << "Time: " << period << " Operation: ";
-
-        if (prob <= 55) { //55% prob car in front pays
-            cout << "Car paid: ";
-            line.front().print(); //peek front and print
-            line.pop_front(); //remove from front
-            cout << endl;
-        }
-        else{ //remaining 45% prob car joins
-            cout << "Joined lane: ";
-            Car nextCar; //create new car
-            line.push_back(nextCar); //put in back of deque
-            nextCar.print();
-            cout << endl;
+            }  
         }
 
-        displayQueue(line);
+        displayPlaza(lanes);
         cout << endl;
-        period++;
-    } */
-        
+    }
+       
 }
 
 void displayQueue(deque<Car>& line) {
     if (line.empty()) {
-        cout << "\tEmpty" << endl;
+        cout << "\tEmpty" << endl << endl;
     }
     else {
         for (Car& car : line) {
